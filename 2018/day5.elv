079987dd969aed35519265435658d99d4982032c
SolitudeSF <solitudesf@protonmail.com>
2018-12-05 16:07:03 +0200

    2018 - 5

---
+++ 2018/day5.elv
@@ -0,0 +1,35 @@
+#!/usr/bin/env elvish
+use str
+
+input = (cat "input/day5")
+
+alpha = [a b c d e f g h i j k l m n o p q r s t u v w x y z]
+to-replace = [(for i [(range (count $alpha))] {
+      upper = (str:to-upper $alpha[$i])
+      put $alpha[$i]$upper $upper$alpha[$i]
+})]
+
+fn reacted [s]{
+  res = $s
+  last = ""
+  while (not-eq $res $last) {
+    last = $res
+    for a $to-replace {
+      res = (replaces $a '' $res)
+    }
+  }
+  put $res
+}
+
+count (reacted $input)
+
+fn without [s c]{
+  replaces $c '' (replaces (str:to-upper $c) '' $s)
+}
+
+min = (count $input)
+for a $alpha {
+  l = (count (reacted (without $input $a)))
+  if (< $l $min) { min = $l }
+}
+put $min
---
+++ 2018/day5.nim
@@ -0,0 +1,38 @@
+import strutils
+
+const file = "input/day5"
+
+let input = file.readFile.strip
+
+func isReplacement(a, b: char): bool =
+  (a == toLowerAscii(b) and isUpperAscii(b) and isLowerAscii(a)) or
+    (a == toUpperAscii(b) and isLowerAscii(b) and isUpperAscii(a))
+
+func reacted(s: string): string =
+  result = s
+  var l = s.len
+  for i in countdown(s.high, 1):
+    if isReplacement(result[i], result[i-1]):
+      for x in i + 1..<l:
+        result[x - 2] = result[x]
+      dec l, 2
+  result.setLen l
+
+echo input.reacted.len
+
+func withoutUnit(s: string, c: char): string =
+  result = newString(s.len)
+  var i = 0
+  for a in s:
+    if a != c and a != c.toUpperAscii:
+      result[i] = a
+      inc i
+  result.setLen i
+
+var min = input.len
+for a in 'a'..'z':
+  let l = input.withoutUnit(a).reacted.len
+  if l < min:
+    min = l
+
+echo min
